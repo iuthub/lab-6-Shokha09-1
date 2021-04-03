2c48f945e7fbe402a7c6a141a5d869259c345039
Shokha09 <shohrukh.shakhidi@mail.ru>
2021-04-03 12:56:59 +0500

    Completed all problems

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -7,18 +7,21 @@
 
 	$match="Not checked yet.";
 
-if ($_SERVER["REQUEST_METHOD"]=="POST") {
+if ($_SERVER["REQUEST_METHOD"]=="POST")
+{
 	$pattern=$_POST["pattern"];
 	$text=$_POST["text"];
-	$replaceText=$_POST["replaceText"];
 
-	$replacedText=preg_replace($pattern, $replaceText, $text);
-
-	if(preg_match($pattern, $text)) {
-						$match="Match!";
-					} else {
-						$match="Does not match!";
-					}
+	if(preg_match($pattern, $text))
+	{
+	    $match="Match!";
+        $removeWhitespace=preg_replace($pattern, $replacedText,$text); // '/\s+/' for removing whitespace
+        $removeNonNumeric=preg_replace($pattern,$replacedText,$text); // /[^0-9,.]/ for removing non numeric
+        $removeNewLine=preg_replace($pattern,$replacedText,trim($text));  // /\s+/ and trim() for removing
+        $extractText=preg_match($pattern, $text, $match1); //   #\[(.*?)\]#
+	} else {
+	    $match="Does not match!";
+	}
 }
 
 ?>
@@ -39,16 +42,28 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 			<dt>Text</dt>
 			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
 
-			<dt>Replace Text</dt>
-			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
 
 			<dt>Output Text</dt>
 			<dd><?=	$match ?></dd>
+            <?php if($pattern=='/\s+/'){ ?>
+                <dt>Removed whitespace</dt>
+                <dd><?=$removeWhitespace ?></dd>
+            <?php } ?>
+            <?php if($pattern=='/[^0-9,.]/'){ ?>
+                <dt>Removed nonnumeric</dt>
+                <dd><?= $removeNonNumeric ?></dd>
+            <?php } ?>
+            <?php if($pattern=='/\s+/'){ ?>
+                <dt>Removed new lines</dt>
+                <dd><?=$removeNewLine ?></dd>
+            <?php } ?>
+            <?php if($pattern=='#\[(.*?)\]#'){ ?>
+                <dt>Extract text</dt>
+                <dd><?=$match1[1] ?></dd>
+            <?php } ?>
 
-			<dt>Replaced Text</dt>
-			<dd> <code><?=	$replacedText ?></code></dd>
 
-			<dt>&nbsp;</dt>
+            <dt>&nbsp;</dt>
 			<dd><input type="submit" value="Check"></dd>
 		</dl>
 
